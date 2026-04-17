57a3ccaf1abd2370d42b6e5d959eb0a7d7d061b2
Diagonalc <diagonalchen@outlook.com>
2026-04-17 23:48:04 +0800

    geezzz...

---
+++ excerice/pta_ds_q8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_NODE 30
+
+typedef struct bnode
+{
+    int val;
+    struct bnode* left;
+    struct bnode* right;
+} node;
+
+
+int pre[MAX_NODE] = {0};
+int in[MAX_NODE] = {0};
+int post[MAX_NODE] = {0};
+int n;
+
+int build_tree()
+{
+    
+}
+
+int main()
+{
+//if n == 1
+    return 0;
+}

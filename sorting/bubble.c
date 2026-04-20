a0fd9c6351a9fd6066e9606d99faaee30e292734
Diagonalc <diagonalchen@outlook.com>
2026-04-20 23:54:59 +0800

    save

---
+++ sorting/bubble.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubble(int *arr, int n)
+{
+    for
+}
+
+int main()
+{
+    int n;
+    int arr[n];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        scanf(" %d", &arr[i]);
+    bubble(arr);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}

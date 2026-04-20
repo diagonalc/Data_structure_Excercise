6b09770b222603bb645c1869c040b8e1041d9900
Diagonalc <diagonalchen@outlook.com>
2026-04-21 00:02:36 +0800

    done

--- sorting/bubble.c
+++ sorting/bubble.c
@@ -3,20 +3,33 @@
 
 void bubble(int *arr, int n)
 {
-    for
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
 int main()
 {
     int n;
     int arr[n];
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
-        scanf(" %d", &arr[i]);
-    bubble(arr);
+    // scanf("%d", &n);
+    // for (int i = 0; i < n; i++)
+    //     scanf(" %d", &arr[i]);
+    n = 10;
+    int a[10] = {5, 3, 7, 1, 4, 9, 8, 0, 2, 6};
+    bubble(a, n);
 
     for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+        printf("%d ", a[i]);
 
     return 0;
 }

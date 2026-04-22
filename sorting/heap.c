c062030da0ee4a9c796a3917b7cbad9f37d6cfdf
Diagonalc <diagonalchen@outlook.com>
2026-04-23 00:03:07 +0800

    unfin

--- sorting/heap.c
+++ sorting/heap.c
@@ -1,1 +1,28 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+#define INT_MAX 65535
+
+typedef struct heap
+{
+    int *arr;
+    int size;
+    int cap;
+} heap;
+
+heap *create_heap(int cap)
+{
+    heap *nh = (heap *)malloc(sizeof(heap));
+    nh->arr = (int *)malloc(cap * sizeof(int));
+    nh->arr[0] = -INT_MAX;
+    nh->size = 0;
+    nh->cap = cap;
+    return nh;
+}
+
+void insert(int val, heap *h)
+{
+    if (h->size == h->cap)
+        return;
+    int i = h->size + 1;
+    for ()
+}

b8a65063f4509a7c1c80679b08d7b02280a6a328
Diagonalc <diagonalchen@outlook.com>
2026-06-13 16:54:58 +0800

    restart!

--- sorting/merge.c
+++ sorting/merge.c
@@ -25,6 +25,7 @@ void merge(int a[], int *tmp, int l, int r, int r_end)
         a[i] = tmp[i];
 }
 
+// 1. recursive
 void msort_core(int a[], int *tmp, int l, int r_end)
 {
     if (l < r_end)
@@ -35,7 +36,6 @@ void msort_core(int a[], int *tmp, int l, int r_end)
         merge(a, tmp, l, center + 1, r_end);
     }
 }
-
 void msort(int a[], int n)
 {
     int *tmp = malloc(n * sizeof(int)); // only need to create and free tmp array for once within the entire sorting, if tmp is created within msort_core then it will be created and freed for many times
@@ -52,10 +52,44 @@ void msort(int a[], int n)
     }
 }
 
+// 2. non-recursive
+void msort_pass(int a[], int tmp[], int n, int length)
+{
+    // length: the length of the current ordered subsequence
+    int i;
+    for (i = 0; i <= n - 2 * length; i += 2 * length)
+    {
+        merge(a, tmp, i, i + length, i + 2 * length - 1);
+    }
+    if (i + length < n)
+        merge(a, tmp, i, i + length, n - 1);
+    else
+        for (int j = i; j < n; j++)
+            tmp[j] = a[j];
+}
+void msort_nonre(int a[], int n)
+{
+    int *tmp = malloc(n * sizeof(int));
+    int length = 1;
+    if (tmp != NULL)
+    {
+        while (n > length)
+        {
+            msort_pass(a, tmp, n, length);
+            length *= 2;
+            msort_pass(tmp, a, n, length);
+            length *= 2;
+        }
+        free(tmp);
+    }
+    else
+        printf("%s", "insufficient space");
+}
+
 int main()
 {
     int arr[10] = {6, 7, 8, 9, 1, 2, 3, 4, 5};
-    msort(arr, 9);
+    msort_nonre(arr, 9);
     for (int i = 0; i < 9; i++)
         printf("%d ", arr[i]);
 

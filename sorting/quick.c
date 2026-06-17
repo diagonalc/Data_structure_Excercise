511c1bec0d6327fb2f95e9fcbc60608806a99112
Diagonalc <diagonalchen@outlook.com>
2026-06-17 23:43:28 +0800

    half

--- sorting/quick.c
+++ sorting/quick.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "insertion.c"
+#define CUTOFF 10
 
 void exchange(int *a, int *b)
 {
@@ -10,9 +12,26 @@ void exchange(int *a, int *b)
 
 void qsort(int a[], int l, int r)
 {
-    if (l >= r)
-        return;
-    int key = l;
-    l++;
-    
+    if (l - r <= CUTOFF)
+    {
+        int pivot = a[l];
+        int i = l+1;
+        int j = r;
+        for (;;)
+        {
+            while(a[++i] < pivot){
+            }
+            while(a[j--] > pivot){
+            }
+            if(i<j){
+                exchange(a[i], a[j]);
+            }
+            else
+                break;
+        }
+    }
+    else
+    {
+        insertion(a, l - r);
+    }
 }
